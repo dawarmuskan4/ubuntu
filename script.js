dbb4a7b2c5b795855e4d4baf1a6f02dc96f64a19
dawarmuskan4 <dawarmuskan4@gmail.com>
2023-07-31 14:15:51 +0530

    function section completed

---
+++ script.js
@@ -0,0 +1,215 @@
+'use strict'
+
+// const oneWord = function(str){
+//     return str.replace(/ /g, '').toLowerCase()
+// }
+
+// const upperFirstWord = function(str){
+//     const [first, ...others] = str.split(' ')
+//     return [first.toUpperCase(), ...others].join(' ')
+// }
+
+// //Higher order function
+// const transformer = function (str, fn){
+//     console.log(`originl string : ${str}`)
+//     console.log(`transformed string: ${fn(str)}`)
+
+//     console.log(`transformed by ${fn.name}`)
+// }
+// transformer('JavaScript is the best', upperFirstWord)
+// transformer('JavaScript is the best', oneWord)
+// const high5= function(){
+//     console.log('5')
+// }
+// document.body.addEventListener('click', high5)
+
+//Function returning function
+// const greet = function(greeting){
+//     return function(name){
+//         console.log(`${greeting} ${name}`)
+//     }
+// }
+
+// const greeterHey = greet('Hey')
+// console.log(greeterHey)
+// greeterHey('Jonas')
+// greeterHey('Steven')
+// greet('Hello')('Muskan')
+
+// //using arrow functions
+
+// const greet1 = greeting => {
+//     return name => {
+//         console.log(`${greeting} ${name}`)
+//     }
+// }
+// const greeter= greet1('hello')
+// greeter('Sonya')
+// greet1("Hey")("Naina")
+
+// //another way wth arrow function
+
+// const greetArr = greeting => name => console.log(`${greeting} ${name}`)
+// greetArr('Hi')('Lovish')
+
+//Call and apply method
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+//     book(flightNum, name){
+//         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+//         this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+//     }
+// }
+
+// lufthansa.book(239, 'Jonas')
+// lufthansa.book(345, 'John')
+
+// const eurowings = {
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     bookings: []
+// }
+// const book = lufthansa.book
+
+// //call method
+// book.call(eurowings, 23, 'Sarah')
+// console.log(eurowings)
+// book.call(lufthansa, 239, 'Mary')
+
+// const swiss = {
+//     airline: "Swiss AirLines",
+//     iataCode: 'LX',
+//     bookings: []
+// }
+// book.call(swiss, 234, 'Mary')
+// console.log(swiss)
+
+// //apply method
+// const flightData = [583, 'George']
+// book.apply(swiss, flightData)
+// console.log(swiss)
+
+// //alternative of apply method using call method
+// book.call(swiss, ...flightData)
+
+// //bind method 
+// const bookEW = book.bind(eurowings)
+// const bookLF = book.bind(lufthansa)
+// const bookSW = book.bind(swiss)
+// bookEW(23, 'Steven Williams')
+
+// //bind method with event listener
+// lufthansa.planes = 300
+// lufthansa.buyPlane = function(){
+//     console.log(this)
+//     this.planes++
+//     console.log(this.planes)
+// }
+// document.querySelector('.buy').addEventListener('click',lufthansa.buyPlane.bind(lufthansa) )
+
+// //partial application
+// const addTax = (rate, value) => value + value*rate
+// console.log(addTax(0.1,200))
+// const addVAT = addTax.bind(null, 0.23)
+// console.log(addVAT(200))
+
+// //alternative way to write above function using technique function returning function
+// const addTAX = function(rate){
+//     return function(value){
+//         console.log(value+value*rate)
+//     }
+// }
+// const addVAT2 = addTAX(0.23)
+// addVAT2(100)
+// addTAX(0.1)(100)
+
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should 
+  look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option 
+  is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the 
+  input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+
+2. Call this method whenever the user clicks the "Answer poll" button.
+
+3. Create a method 'displayResults' which displays the poll results. The method takes a string 
+as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', 
+simply display the results array as it is, using console.log(). This should be the default 
+option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' 
+and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look 
+like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+// const poll = {
+//     question: "what is your fav programming language",
+//     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//     answers: new Array(4).fill(0),
+//     registerNewAnswer(){
+//         const ans = Number(prompt(`${this.question}\n ${this.options.join('\n')}(Write option number)`))
+//         if(ans >= 0 && ans <=3){
+//             this.answers[ans]++
+//         }
+        
+//         //console.log(this.answers)
+//         this.displayResult()
+//         this.displayResult('string')
+//     },
+//     displayResult(type = 'array'){
+//         if(type === 'array'){
+//             console.log(this.answers)
+//         } else if(type === 'string'){
+//             console.log(`Poll results are ${this.answers.join(', ')}`)
+//         }
+//     }
+// }
+
+// document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+// poll.displayResult.call({answers: [5,2,3]}, 'string')
+// poll.displayResult.call({answers: [1,3,4,5,1,1]})
+
+
+
+// (function(){
+//     console.log('this will never run again')
+// })()
+// (() => console.log('lllll'))()
+
+const secureBooking = function(){
+  let passengerCount = 0;
+
+  return function(){
+    passengerCount++;
+    console.log(`${passengerCount} passengers`)
+  }
+}
+
+const booker = secureBooking()
